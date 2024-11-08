55438e68411a2536ba0e988c0785524d1334fa42
GeraldineGomez <avocadoenterprise@gmail.com>
2024-11-08 12:20:44 -0500

    feat: add plot function of historic data

--- R/plotting_data.R
+++ R/plotting_data.R
@@ -268,3 +268,88 @@ plot_table_epiweek_tosferina <- function(data_epiweek, distribution_epiweek) {
     kableExtra::kable_styling()
   return(table_epiweek)
 }
+
+#' @title Graficar el tiempo epidemiológico historico
+#' @export
+plot_historic_epi_time <- function(stacked_data, tabla,
+                                   line_data) {
+  scaling_factor <- 700 / 70
+  # Plot the figure
+  ggplot2::ggplot() +
+    # Stacked bar chart
+    ggplot2::geom_bar(data = stacked_data, 
+             ggplot2::aes(x = YearWeek, y = Cases, fill = Virus_Type), 
+             stat = "identity",
+             width = 0.4) +
+    
+    # Line chart for % positivity with scaling applied
+    ggplot2::geom_line(data = line_data, 
+                       ggplot2::aes(x = YearWeek, 
+                  y = Percent_Positivity * scaling_factor, 
+                  color = "Positivity Rate", 
+                  group = 1),
+              color = "#E97132",
+              linewidth = 0.7) +
+    
+    # Scale and labels with specified y-axis breaks
+    ggplot2::scale_y_continuous(name = "NÚMERO DE CASOS POSITIVOS",
+                       limits = c(-500, 700), breaks = seq(0, 700, by = 100),
+                       sec.axis = ggplot2::sec_axis(~ . / scaling_factor, 
+                                           breaks = seq(0, 70, by = 10), 
+                                           labels = function(x) sprintf("%.1f", x))
+    ) +
+    ggplot2::scale_x_discrete(labels = tabla$periodo_epidemiologico)+
+    ggplot2::scale_fill_manual(values = c(
+      "a_h1n1_pdm09" = "#8064A2",       # Light blue for A(H1N1)pdm09
+      "a_no_subtipificado" = "#4BACC6", # Purple for A no subtipificado
+      "a_h3" = "#F79646",              # Green for A(H3)
+      "influenza_B" = "#2C4D75",       # Dark gray for Influenza B
+      "adenovirus" = "#4D3B62",         # Dark teal for Adenovirus
+      "metapneumovirus" = "#2C4D75",    # Dark purple for Metapneumovirus
+      "rinovirus" = "#B65708",          # Dark green for Rinovirus
+      "bocavirus" = "#729ACA",          # Blue for Bocavirus
+      "otros_virus" = "#4F81BD",        # Blue for Otros Virus
+      "parainfluenza" = "#772C2A",      # Brown for Parainfluenza
+      "vsr" = "#5F7530",                # Dark green for VSR
+      "nueva_columna" = "black"         # Black for nueva_columna (appears as line in legend)
+    )) +
+    ggplot2::labs(x = "PERÍODO EPIDEMIOLOGICO", fill = NULL, color = NULL) +
+    
+    # Customize the grid lines
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_text(angle = 0, size = 7,
+                                          margin = ggplot2::margin(t = -255, b=-5)),
+      axis.title.x = ggplot2::element_text(margin = ggplot2::margin(t = 20, b=-10),
+                                           size = 8, face = "bold",
+                                           color = "#595959"),
+      axis.title.y = ggplot2::element_text(hjust = 0.75, size = 8,
+                                           face = "bold", color = "#595959"),
+      panel.grid.major.x = ggplot2::element_blank(),         # Remove vertical major grid lines
+      panel.grid.minor.x = ggplot2::element_blank(),         # Remove vertical minor grid lines
+      panel.grid.minor.y = ggplot2::element_blank(),
+      legend.position = "bottom",
+      legend.key.size = ggplot2::unit(1.2, "lines"),
+      legend.key.height = ggplot2::unit(0.02, "lines"),
+      legend.text = ggplot2::element_text(size = 7)
+    ) +
+    # Agregar líneas verticales con altura ajustable usando geom_segment
+    ggplot2::geom_segment(ggplot2::aes(x = 13.5, xend = 13.5, y = -25, yend = 700),
+                          color = "black", linewidth = 0.65) +
+    ggplot2::geom_segment(ggplot2::aes(x = 26.5, xend = 26.5, y = -25, yend = 700),
+                          color = "black", linewidth = 0.65) +
+    ggplot2::annotate("text", x = c(6, 19, 31), y = -35, label = c("2022",
+                                                                   "2023",
+                                                                   "2024"),
+                      size = 2.4, fontface = "bold") +
+    ggplot2::guides(
+      fill = ggplot2::guide_legend(
+        nrow = 3,
+        byrow = TRUE
+      ),
+      color = "none") + 
+    ggplot2::annotate("segment", x = 21, xend = 22.2, y = -150, yend = -150,
+                      color = "#E97132", linewidth = 0.7) +
+    ggplot2::annotate("text", x = 22.5, y = -150, label = "% DE POSITIVIDAD",
+                      hjust = 0, color = "black", size= 2) 
+}
