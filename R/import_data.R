a255c0f934f9c4b074377eb2e54944d2ae8a01e7
GeraldineGomez <avocadoenterprise@gmail.com>
2024-11-08 07:37:15 -0500

    feat: add import functions

--- R/import_data.R
+++ R/import_data.R
@@ -1,20 +1,27 @@
-import_data_viral_circulation <- function(report_data = c(), header = FALSE) {
-  
+#' @title Importar los datos de la circulación viral
+#' @export
+import_data_viral_circulation <- function(report_data = NULL,
+                                          header = FALSE,
+                                          skip = 3,
+                                          col_names = FALSE,
+                                          sheet) {
   viral_circulation_data <- data.frame()
-  
   for (data_path in report_data) {
     file_extension <- tools::file_ext(data_path)
     if (!is.null(file_extension)) {
-      temp_data <- switch(  
-        file_extension,  
-        "xlsx" = readxl::read_excel(data_path, col_names = F, skip = 3),  
-        "csv" = utils::read.csv(data_path, header = header, skip = if (header) 0 else 3)
+      temp_data <- switch(
+        file_extension,
+        "xlsx" = readxl::read_excel(data_path,
+                                    col_names = col_names,
+                                    skip = skip,
+                                    sheet = sheet),
+        "csv" = utils::read.csv(data_path, header = header,
+                                skip = if (header) 0 else 3)
       )
       if (!header) {
         temp_data <- row_to_header(data = temp_data)
         viral_circulation_data <- rbind(viral_circulation_data, temp_data)
-      }
-      else {
+      } else {
         viral_circulation_data <- temp_data
       }
     }
